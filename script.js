0829a38fee2d94a3e5f309a9b0c7511529dc7fc3
Amanda Samuelsson <AmandasMacbook@Amandas-MacBook.local>
2020-10-29 15:14:17 +0100

    Updated from alert/prompt to input/buttons

--- script.js
+++ script.js
@@ -1,69 +1,95 @@
-let currentScene = 0 
+let currentSceneName = "mountain" 
 
-const scenes = [
-   
-    {
-        descritption: "..och du befinner dig på en bergstopp, skrika eller gå neråt. Skriv in skrik eller gå neråt",
+const scenes = {
+    
+    mountain: {
+        description: "Du befinner dig på en bergstopp, skrika eller gå neråt. Skriv in skrik eller gå neråt",
         choices: ["skrik", "gå neråt"],
-        nextScene: [1, 2]
+        nextScene: ["fly", "goDown"]
     },
-    {
-        descritption: "Du valde att skrika och en Grip kommer nu och lyfter dig i sina klor. Du funderar på om du ska slå mot klorna med din dolk eller om du ska njuta av utsikten. Vad väljer du att göra?",
-        choices: ["slå", "njuta utsikten"],
-        nextScene: [2, 1]
+    
+    fly: {
+        description: "Du valde att skrika och en Grip kommer nu och lyfter dig i sina klor. Du funderar på om du ska slå mot klorna med din dolk eller om du ska njuta av utsikten. Vad väljer du att göra?",
+        choices: ["slå", "njut av utsikten"],
+        nextScene: ["hit", "view"]
     },
-    {
-        descritption: "Du valde att slå, Gripen släpper taget. Du faller nu handlöst 400 meter och störtar snabbt mot din död. Game over. Vill du börja om?",
-        choices: ["ja", "nej"],
-        nextScene: [0, 4]
+    
+    goDown: {
+        description: "Du börjar gå ner för berget men ramlar ner i en glaciärspricka och dör omedelbart. Vill du börja om?",
+        choices: ["ja"],
+        nextScene: ["mountain"]
+    },
+    
+    view: {
+        description: "Gripen flyger vidare med dig i sina klor, efter ett tag börjar gripen flyga lägre och du ser en stad. Du känner igen staden från",
+        choices: ["ett av dina tidigare äventyr", "en berättelse du hört av din farfar"],
+        nextScene: ["hit", 1]
+    },
+    
+    hit: {
+        description: "Du valde att slå, Gripen släpper taget. Du faller nu handlöst 400 meter och störtar snabbt mot din död. Vill du börja om?",
+        choices: ["ja"],
+        nextScene: ["mountain"]
     }
-];
+};
 
-window.onload = submitName;
-
-function submitName() {
-    alert ("Du kommer nu få spela ett äventyr där du själv kommer få göra ett antal val, hur äventyret slutar avgör du. Lycka till!");
-    let Name = prompt("Skriv in ditt namn");
-    while (Name === "") {
-    alert("Vänligen skriv in ditt namn")
-    let Name = prompt("Skriv in ditt namn");
-    } 
-    alert("Hej och välkommen " + Name);
-    alert("Du är en erfaren äventyrare och har varit på många, långa äventyr med dina vapendragare Tola Sparv och Monika Plöjer men nu vaknar du upp..")
+window.onload = presentScene;
 
 
+// function submitName() {
+    //     alert ("Du kommer nu få spela ett äventyr där du själv kommer få göra ett antal val, hur äventyret slutar avgör du. Lycka till!");
+    //     let Name = prompt("Skriv in ditt namn");
+    //     while (Name === "") {
+        //     alert("Vänligen skriv in ditt namn")
+        //     let Name = prompt("Skriv in ditt namn");
+        //     } 
+        //     alert("Hej och välkommen " + Name);
+        //     alert("Du är en erfaren äventyrare och har varit på många, långa äventyr med dina vapendragare Tola Sparv och Monika Plöjer men nu vaknar du upp..")
+        // }
+
+// function submitName() {
+//     let name = 
+// }
+        
 function presentScene() {
-    const answer = prompt(scenes[currentScene].descritption);
-    handleUserChoice(answer)
-
+    const scene = scenes[currentSceneName];
+    updateDescription(scene);
+    createChoices(scene);
+    
 }
+        
+function updateDescription(scene) {
+    const descriptionTag = document.getElementById('description');
+    descriptionTag.innerText = scene.description;
+    
 }
 
-function handleUserChoice(answer) {
-    console.log(answer);
+function createChoices(scene) {
+    const container = document.getElementById('choices-container');
+    container.innerText = "";
+    //container.innerText = scene.choices;
+    
+    for (let i = 0; i < scene.choices.length; i++) {
+        const choice = scene.choices[i];
+        const nextScene = scene.nextScene[i];
+        createButton(choice, nextScene, container);
 
-    if (answer === scenes[currentScene].choices[0]) {
-        currentScene = scenes[currentScene].nextScene[0]
     }
+}
 
-    if (answer === scenes[currentScene].choices[1]) {
-        currentScene = scenes[currentScene].nextScene[1]
+function createButton(choice, nextScene, container) {
+    const button = document.createElement('button'); 
+    button.innerText = choice, nextScene;
+    button.onclick = function() {
+        handleUserChoice(nextScene);
+        
     }
+    container.append(button);
+}
 
-
-    // else if (answer !== scenes[currentScene].choices[0, 1, 2, 3, 4]) {
-    //     alert("Välkommen")
-    // }
-  
-
-    // else if(answer !== scenes[currentScene].choices[0]); {
-    //     alert("Felaktig inmatning"); 
-    //     scenes[currentScene].nextScene[0]
-    // }
-    // while (answer !== scenes[currentScene].choices[0]); {
-    //     alert("Felaktig inmatning"); 
-    //     scenes[currentScene].nextScene[0]
-    // }
-
-    presentScene()
+function handleUserChoice(nextScene) {
+    
+    console.log("Hej")
+    currentSceneName = nextScene;
+    presentScene();
 }
